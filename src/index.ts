afbedd7c248a078f5028a6120a9a0413ec202eda
Renato <renatopcinfo@gmail.com>
2021-09-19 14:06:47 -0300

    feat: add status route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
 
 const app = express()
@@ -6,8 +7,9 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(statusRoute)
 app.use(usersRoute)
 
 app.listen(3000, () => {
-    console.log('Server rodando na porta 3000');
+    console.log('Server rodando na porta 3000!');
 })
