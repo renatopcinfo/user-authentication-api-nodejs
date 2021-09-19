0249c79fc737ccae9234a00c19a89f1d7154c3d0
Renato <renatopcinfo@gmail.com>
2021-09-18 22:57:16 -0300

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import express, { Request, Response, NextFunction } from 'express';
 
-console.log('teste')
+const app = express()
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar'})
+})
+
+app.listen(3000, () => {
+    console.log('Server rodando porta 3000.');
+})
