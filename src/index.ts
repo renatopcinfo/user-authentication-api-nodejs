2fc103ae1649ce0c40e47297af812b174e421dbe
Renato <renatopcinfo@gmail.com>
2021-11-27 20:33:41 -0300

    feat: add bearer token validation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import bearerAuthenticationMiddleware from './middlewares/bearer-authentication.middleware';
 import errorHandler from './middlewares/error-handler.middleware';
 import authorizationRoute from './routes/authorization.route';
 import statusRoute from './routes/status.route';
@@ -12,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //Routes
 app.use(statusRoute)
-app.use(usersRoute)
+app.use(bearerAuthenticationMiddleware, usersRoute)
 app.use(authorizationRoute)
 
 //Handlers errors
