5f8c5e032c6b960a809f183dcd3500e1cae35324
Renato <renatopcinfo@gmail.com>
2021-11-27 19:43:08 -0300

    feat: add middleware

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,47 +1,24 @@
 import { StatusCodes } from 'http-status-codes';
 import { Router, Request, Response, NextFunction } from 'express'
-import ForbiddenError from '../models/errors/forbidden.error.model'
-import userRepository from '../repositories/user.repository'
 import JWT from 'jsonwebtoken'
+import basicAuthenticationMiddleware from '../middlewares/basic-authentication.middleware';
+import ForbiddenError from '../models/errors/forbidden.error.model';
 
 const authorizationRoute = Router()
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
+authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const authorizationHeader = req.headers['authorization']
+    const user = req.user
 
-    if(!authorizationHeader) {
-      throw new ForbiddenError('Credencias não informadas')
+    if(!user) {
+      throw new ForbiddenError('Usuário não informado!')
     }
-
-    //Basic YWRtaW46YWRtaW4
-    const [authenticationType, token] = authorizationHeader.split(' ')
-
-    if (authenticationType !== 'Basic' || !token) {
-      throw new ForbiddenError('Tipo de autenticação inválida!')
-    }
-
-    const tokenContent = Buffer.from(token, 'base64').toString('utf-8')
-
-    const [username, password] = tokenContent.split(':')
-
-    if(!username || !password) {
-      throw new ForbiddenError('Credenciais não preenchidas')
-    }
-
-    const user = await userRepository.findByUsernameAndPassword(username, password)
-
-    if (!user) {
-      throw new ForbiddenError('Usuário ou senha inválidos!')
-  }
-
     const jwtPayload = { username: user.username }
     const jwtOptions = { subject: user?.uuid }
     const secretKey = 'my_secret_key'
 
     const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions)
     res.status(StatusCodes.OK).json({ token: jwt })
-
 
   } catch (error) {
     next(error)
